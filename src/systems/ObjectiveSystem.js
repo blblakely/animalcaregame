69b271ee18f34d83a6783d4745fbfa1803cb72c7
blblakely <brookeiceskater@gmail.com>
2026-08-23 12:48:47 -0700

    Add src/systems/ObjectiveSystem.js

---
+++ src/systems/ObjectiveSystem.js
@@ -0,0 +1,5 @@
+export class ObjectiveSystem {
+  constructor(){ this.items=[['feed','Feed an animal'],['play','Play with an animal'],['clean','Clean an enclosure'],['profile','View an animal profile'],['adopt','Complete an adoption']].map(([id,label])=>({id,label,done:false})); }
+  complete(id){ const item=this.items.find(i=>i.id===id); if(item&&!item.done){item.done=true;return true} return false; }
+}
+
